20fb3b52497840aa0c83f4c593d3060c6945fce8
stella05925 <stella05925@outlook.com>
2024-09-08 19:04:58 -0700

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,8 @@
-// save the data: data structure
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-}, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}];
+// save the data: data structure (in data/products.js)
 
 let productsHTML = '';
 
-// generate the HTML
+// generate the HTML using the data
 products.forEach((product) => {
   productsHTML += `
     <div class="product-container">
